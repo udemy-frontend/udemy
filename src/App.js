5c4a4782a8d661f280377be9c382691f7f6ab15e
unknown <elnino2604@gmail.com>
2020-06-30 14:49:22 +0700

    cm codecode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from "react";
-<<<<<<< HEAD
 // import Headers from "./features/Header/Headers";
 import { Provider } from "react-redux";
 import configureStore from "./components/Redux/configStore";
 import Home from "./components/core/Home";
-=======
-import Headers from "./features/Header/Headers";
-import { Provider } from "react-redux";
-import configureStore from "./components/Redux/configStore";
-
->>>>>>> f0198d3cefea50c8b23d56cec43b025fa5f123e9
 const store = configureStore();
 function App() {
   return (
     <Provider store={store}>
-<<<<<<< HEAD
       <Home />
-=======
-      <Headers />
->>>>>>> f0198d3cefea50c8b23d56cec43b025fa5f123e9
     </Provider>
   );
 }
